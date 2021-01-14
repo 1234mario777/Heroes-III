8a2b2fda39c9f0f63376d8f0472f1deb7da6f014
JakubPrzybyla <kubaprzybya@gmail.com>
2021-01-14 23:12:20 +0100

    test shop calculator

---
+++ economy-model/src/main/java/pl/sdk/creatures/CreatureShopCalculator.java
@@ -0,0 +1,18 @@
+package pl.sdk.creatures;
+
+import pl.sdk.hero.EconomyHero;
+
+import java.util.Random;
+
+public class CreatureShopCalculator
+{
+	public CreatureShopCalculator( Random aRand )
+	{
+
+	}
+
+	int calculateMaxAmount( EconomyHero aHero1, EconomyCreature aCreature )
+	{
+		return 0;
+	}
+}
---
+++ economy-model/src/test/java/pl/sdk/creatures/CreatureShopCalculatorTest.java
@@ -0,0 +1,54 @@
+package pl.sdk.creatures;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.sdk.EconomyEngine;
+import pl.sdk.hero.EconomyHero;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CreatureShopCalculatorTest
+{
+	private final EconomyNecropolisFactory creatureFactory = new EconomyNecropolisFactory();
+	private Random rand;
+	EconomyCreature creature;
+	@BeforeEach
+	void init() {
+		creature = creatureFactory.create(false, 1, 1);
+	}
+
+	@Test
+	void shouldCorrectlyCalculateMaxAmountToBuyWhenGrowthIsSmallerThanPurchaseOpportunity()
+	{
+		EconomyHero hero1 = new EconomyHero(EconomyHero.Fraction.NECROPOLIS, 3000);
+		rand = mock(Random.class);
+		when( rand.nextDouble() ).thenReturn( 0.5 );
+		CreatureShopCalculator calculator = new CreatureShopCalculator(rand);
+		assertEquals( 9,  calculator.calculateMaxAmount( hero1, creature ));
+	}
+
+	@Test
+	void shouldCorrectlyCalculateMaxAmountToBuyWhenGrowthIsBiggerThanPurchaseOpportunity()
+	{
+		EconomyHero hero = new EconomyHero(EconomyHero.Fraction.NECROPOLIS, 600);
+		rand = mock(Random.class);
+		when( rand.nextDouble() ).thenReturn( 1.0 );
+		CreatureShopCalculator calculator = new CreatureShopCalculator(rand);
+		assertEquals( 10,  calculator.calculateMaxAmount( hero, creature ));
+	}
+
+	@Test
+	void shouldCorrectlyCalculateMaxAmountToBuyWhenGrowthEqualsPurchaseOpportunity()
+	{
+		EconomyHero hero = new EconomyHero(EconomyHero.Fraction.NECROPOLIS, 300);
+		rand = mock(Random.class);
+		when( rand.nextDouble() ).thenReturn( 1.0 );
+		CreatureShopCalculator calculator = new CreatureShopCalculator(rand);
+		assertEquals( 5,  calculator.calculateMaxAmount( hero, creature ));
+	}
+
+}
