6d32f02061c5b5a464d0fe176373cf89084e25c8
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2021-01-15 12:06:12 +0100

    Add masteries to summon spells

--- eco-battle-converter/src/main/java/pl/sdk/converter/spells/SummonSpellFactory.java
+++ eco-battle-converter/src/main/java/pl/sdk/converter/spells/SummonSpellFactory.java
@@ -5,13 +5,22 @@ import pl.sdk.spells.AbstractSpell;
 import pl.sdk.spells.EconomySpell;
 import pl.sdk.spells.SummonSpell;
 
-class SummonSpellFactory extends SpellFactory{
+class SummonSpellFactory extends SpellFactory {
 
     @Override
     AbstractSpell createInner(EconomySpell aEs, int aHeroPower, SpellMasteries aMasteries) {
         switch (aEs.getSpellStatistic()) {
             case SUMMON_AIR_ELEMENTAL:
-                return new SummonSpell(25, aHeroPower, 2 * aHeroPower, "Air Elemental", aEs.getElement());
+                switch (aMasteries.getAir()) {
+                    case BASIC:
+                        return new SummonSpell(25, aHeroPower, 2 * aHeroPower, "Air Elemental", aEs.getElement());
+                    case ADVANCED:
+                        return new SummonSpell(25, aHeroPower, 3 * aHeroPower, "Air Elemental", aEs.getElement());
+                    case MASTER:
+                        return new SummonSpell(25, aHeroPower, 4 * aHeroPower, "Air Elemental", aEs.getElement());
+                    default:
+                        throw new UnsupportedOperationException("Cannot recognize mastery level");
+                }
             default:
                 throw new UnsupportedOperationException("Cannot recognize spell");
         }
