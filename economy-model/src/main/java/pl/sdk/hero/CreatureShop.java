1f1701188556a352ae17168335f4ca3e31aa9529
JakubPrzybyla <kubaprzybya@gmail.com>
2021-01-16 17:43:23 +0100

    can init creature population

--- economy-model/src/main/java/pl/sdk/hero/CreatureShop.java
+++ economy-model/src/main/java/pl/sdk/hero/CreatureShop.java
@@ -1,21 +1,55 @@
 package pl.sdk.hero;
 
 import pl.sdk.creatures.EconomyCreature;
+import pl.sdk.creatures.EconomyNecropolisFactory;
 
+import java.util.HashMap;
 import java.util.Random;
 
 public class CreatureShop {
 
     private CreatureShopCalculator calculator;
+    private final HashMap<Integer, Integer> heroOnePopulation;
+    private final HashMap<Integer, Integer> heroTwoPopulation;
+    private HashMap<Integer, Integer> currentPopulation;
+    private final EconomyNecropolisFactory creatureFactory = new EconomyNecropolisFactory();
 
     public CreatureShop()
     {
         calculator = new CreatureShopCalculator(  );
+        heroOnePopulation = new HashMap<>();
+        heroTwoPopulation = new HashMap<>();
+        currentPopulation = new HashMap<>();
+        createPopulation(heroOnePopulation);
+        createPopulation(heroTwoPopulation);
+        currentPopulation = heroOnePopulation;
     }
 
     CreatureShop( Random aRand )
     {
         calculator = new CreatureShopCalculator(aRand);
+        heroOnePopulation = new HashMap<>();
+        heroTwoPopulation = new HashMap<>();
+        currentPopulation = new HashMap<>();
+        createPopulation(heroOnePopulation);
+        createPopulation(heroTwoPopulation);
+        currentPopulation = heroOnePopulation;
+    }
+
+    private void createPopulation( HashMap<Integer, Integer> aPopulationMap )
+    {
+        aPopulationMap.put( 1, calculatePopulation(1) );
+        aPopulationMap.put( 2, calculatePopulation(2) );
+        aPopulationMap.put( 3, calculatePopulation(3) );
+        aPopulationMap.put( 4, calculatePopulation(4) );
+        aPopulationMap.put( 5, calculatePopulation(5) );
+        aPopulationMap.put( 6, calculatePopulation(6) );
+        aPopulationMap.put( 7, calculatePopulation(7) );
+    }
+
+    private Integer calculatePopulation( int aTier )
+    {
+        return creatureFactory.create( false, aTier, 1 ).getGrowth();
     }
 
     public void buy(EconomyHero aHero, EconomyCreature aEconomyCreature) {
@@ -37,6 +71,6 @@ public class CreatureShop {
 
     public int getCurrentPopulation( int aTier )
     {
-        return 0;
+        return currentPopulation.get( aTier );
     }
 }
