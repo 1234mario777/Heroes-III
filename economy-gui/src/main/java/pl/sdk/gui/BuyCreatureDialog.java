82cf6a2b706c0fd35e2db626d50380940a0b975a
JakubPrzybyla <kubaprzybya@gmail.com>
2021-01-17 18:23:06 +0100

    improved top pane in buying creature dialog

--- economy-gui/src/main/java/pl/sdk/gui/BuyCreatureDialog.java
+++ economy-gui/src/main/java/pl/sdk/gui/BuyCreatureDialog.java
@@ -5,6 +5,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Modality;
@@ -25,7 +27,7 @@ class BuyCreatureDialog
 	void startDialog() {
 		VBox centerPane = new VBox();
 		HBox bottomPane = new HBox();
-		HBox topPane = new HBox();
+		VBox topPane = new VBox();
 		Stage dialog = prepareWindow(centerPane, bottomPane, topPane);
 		Slider slider = creatureSlider.createSlider();
 		prepareConfirmAndCancelButton(bottomPane, slider);
@@ -40,23 +42,33 @@ class BuyCreatureDialog
 		return creatureSlider.getCreatureAmount();
 	}
 
-	private void prepareTop(HBox aTopPane, Slider aSlider) {
-		//TODO creature cops should be visible here
-		aTopPane.getChildren().add(new Label("Single Cost: " + "0") );
-		Label slideValueLabel = new Label("0");
-		aSlider.valueProperty().addListener((slider, aOld, aNew) -> slideValueLabel.setText(String.valueOf(aNew.intValue())));
-		aTopPane.getChildren().add(slideValueLabel);
-		aTopPane.getChildren().add(new Label ("Purchase Cost: "));
+	private void prepareTop(VBox aTopPane, Slider aSlider) {
+
+		Label recruitLabel = new Label("Recruit " + creatureName );
+		recruitLabel.getStyleClass().add("buy-creature-text");
+		aTopPane.getChildren().add(recruitLabel);
+
+		ImageView image = new ImageView(new Image(getClass().getResourceAsStream("/graphics/creatures/" + creatureName + ".png" )));
+		image.setFitHeight(200);
+		image.setFitWidth(200);
+		aTopPane.getChildren().add(image);
+		aTopPane.setAlignment( Pos.CENTER );
+//		//TODO creature cops should be visible here
+//		aTopPane.getChildren().add(new Label("Single Cost: " + "0") );
+//		Label slideValueLabel = new Label("0");
+//		aSlider.valueProperty().addListener((slider, aOld, aNew) -> slideValueLabel.setText(String.valueOf(aNew.intValue())));
+//		aTopPane.getChildren().add(slideValueLabel);
+//		aTopPane.getChildren().add(new Label ("Purchase Cost: "));
 	}
 
 	private Stage prepareWindow( Pane aCenter, Pane aBottom, Pane aTop ) {
 		dialog = new Stage();
 		BorderPane pane = new BorderPane();
-		Scene scene = new Scene(pane, 500,300);
+		Scene scene = new Scene(pane, 700,700);
 		scene.getStylesheets().add("fxml/main.css");
 		dialog.setScene(scene);
 		dialog.initModality( Modality.APPLICATION_MODAL );
-		dialog.setTitle("Buying " + creatureName );
+		dialog.setTitle("Recruit " + creatureName );
 
 		pane.setTop(aTop);
 		pane.setCenter(aCenter);
