525d6f9597189f65d7872fb1392cd13d08827a6c
JakubPrzybyla <kubaprzybya@gmail.com>
2021-01-20 20:57:40 +0100

    added Player class

---
+++ economy-model/src/main/java/pl/sdk/hero/Player.java
@@ -0,0 +1,13 @@
+package pl.sdk.hero;
+
+public class Player
+{
+	EconomyHero hero;
+	CreatureShop creatureShop;
+
+	public Player( EconomyHero aHero, CreatureShop aCreatureShop )
+	{
+		hero = aHero;
+		creatureShop = aCreatureShop;
+	}
+}
