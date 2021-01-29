69664d0e80be859e7f2804884e6c146d233c38dd
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2021-01-29 12:19:40 +0100

    add creature rendering to casting spell mode

--- battle-gui/src/main/java/pl/sdk/gui/BattleMapController.java
+++ battle-gui/src/main/java/pl/sdk/gui/BattleMapController.java
@@ -47,7 +47,7 @@ public class BattleMapController implements PropertyChangeListener {
         gameEngine = new GameEngine(new Hero(notUpgradedCreatures), new Hero(upgradedCreatures));
     }
 
-    public BattleMapController(Hero aHero1, Hero aHero2){
+    public BattleMapController(Hero aHero1, Hero aHero2) {
         gameEngine = new GameEngine(aHero1, aHero2);
     }
 
@@ -61,7 +61,7 @@ public class BattleMapController implements PropertyChangeListener {
             gameEngine.pass();
         });
 
-        spellBookButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) ->{
+        spellBookButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
             SpellChooserDialog spellChooser = new SpellChooserDialog(gameEngine.getActiveHero().getSpells(), gameEngine.getActiveHero().getMana());
             spellChooser.startDialog(this::prepareToCastSpell);
         });
@@ -76,42 +76,44 @@ public class BattleMapController implements PropertyChangeListener {
             for (int y = 0; y < 15; y++) {
                 MapTile rec = new MapTile();
                 gridMap.add(rec, x, y);
-                if (spellToCast == null){
-                    prepareTile(x, y, rec);
+                Creature c = gameEngine.get(x, y);
+                if (c != null) {
+                    boolean shouldFlip = gameEngine.isHeroTwoCreature(c);
+                    rec.addCreature(c.getName(), c.getAmount(), shouldFlip);
                 }
-                else{
-                    prepareTileWithSpells(x,y, rec, spellToCast);
+
+                if (spellToCast == null) {
+                    prepareTile(x, y, rec);
+                } else {
+                    prepareTileWithSpells(x, y, rec, spellToCast);
                 }
             }
         }
     }
 
     private void prepareTileWithSpells(int aX, int aY, MapTile aRec, AbstractSpell aSpellToCast) {
-        if (gameEngine.canCastSpell(aSpellToCast, new Point(aX,aY))){
+        if (gameEngine.canCastSpell(aSpellToCast, new Point(aX, aY))) {
             aRec.changeState(new MapTileCastSpellPossibleState());
-        };
+        }
     }
 
     private void prepareTile(int aX, int aY, MapTile aRec) {
-        gameEngine.addObserver( AFTER_MOVE, aRec );
-        gameEngine.addObserver( AFTER_ATTACK, aRec );
+        gameEngine.addObserver(AFTER_MOVE, aRec);
+        gameEngine.addObserver(AFTER_ATTACK, aRec);
         Creature c = gameEngine.get(aX, aY);
         if (c != null) {
-            boolean shouldFlip = gameEngine.isHeroTwoCreature(c);
-            aRec.addCreature(c.getName(), c.getAmount(), shouldFlip);
-
             if (c == gameEngine.getActiveCreature()) {
-                aRec.changeState( new MapTileActiveCreatureState() );
+                aRec.changeState(new MapTileActiveCreatureState());
             } else if (gameEngine.canAttack(aX, aY)) {
                 final int x1 = aX;
                 final int y1 = aY;
-                aRec.changeState( new MapTileAttackPossibleState() );
+                aRec.changeState(new MapTileAttackPossibleState());
                 aRec.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> gameEngine.attack(x1, y1));
             }
         } else if (gameEngine.canMove(aX, aY)) {
             final int x1 = aX;
             final int y1 = aY;
-            aRec.changeState( new MapTileMovePossibleState() );
+            aRec.changeState(new MapTileMovePossibleState());
             aRec.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> gameEngine.move(new Point(x1, y1)));
         }
     }
