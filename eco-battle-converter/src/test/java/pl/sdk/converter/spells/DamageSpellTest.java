9d943ab03e4b15f449a430a37c3929248c363680
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2021-02-16 12:43:00 +0100

    refactoring

--- eco-battle-converter/src/test/java/pl/sdk/converter/spells/DamageSpellTest.java
+++ eco-battle-converter/src/test/java/pl/sdk/converter/spells/DamageSpellTest.java
@@ -1,29 +1,22 @@
 package pl.sdk.converter.spells;
 
-import com.google.common.collect.Range;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.sdk.Fraction;
-import pl.sdk.Hero;
 import pl.sdk.converter.SpellMasteries;
 import pl.sdk.creatures.*;
 import pl.sdk.spells.DamageSpell;
 import pl.sdk.spells.EconomySpell;
 import pl.sdk.spells.SpellStatistic;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DamageSpellTest {
 
-    private EconomyTestFractionFactory creatureFactory;
     private Creature creatureForTesting;
 
     @BeforeEach
     void init(){
-        creatureFactory = new EconomyTestFractionFactory();
         creatureForTesting = prepareCreatureWith1kHP();
     }
 
