b6d0fce2027648a6e4caaa72e44dbb1457e5969e
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2021-02-17 07:54:32 +0100

    I forgot how to work with often commiting :( range damage spell

--- battle-model/src/main/java/pl/sdk/GameEngine.java
+++ battle-model/src/main/java/pl/sdk/GameEngine.java
@@ -171,7 +171,22 @@ public class GameEngine {
 
     public void castSpell(AbstractSpell aSpell, Point aPoint) {
         queue.getActiveHero().castSpell(aSpell);
-        aSpell.cast(board.get(aPoint));
+        int range = aSpell.getSplashRange();
+        for (int i = -range ; i < range+1; i++) {
+            for (int j = -range; j < range+1; j++) {
+                int x = aPoint.getX() + i;
+                int y = aPoint.getY() + j;
+                if (x < 0 || x > BOARD_WIDTH || y < 0 || y > BOARD_HEIGHT){
+                    continue;
+                }
+
+                if ( board.get(x,y) != null){
+                    aSpell.cast(board.get(x,y));
+                    aSpell.getSplashRange();
+                }
+            }
+        }
+
     }
 
     boolean isAllyCreature(Point aP) {
--- eco-battle-converter/src/test/java/pl/sdk/converter/spells/DamageSpellTest.java
+++ eco-battle-converter/src/test/java/pl/sdk/converter/spells/DamageSpellTest.java
@@ -2,14 +2,24 @@ package pl.sdk.converter.spells;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+
 import pl.sdk.Fraction;
+import pl.sdk.GameEngine;
+import pl.sdk.Hero;
+import pl.sdk.Point;
 import pl.sdk.converter.SpellMasteries;
 import pl.sdk.creatures.*;
 import pl.sdk.spells.DamageSpell;
 import pl.sdk.spells.EconomySpell;
 import pl.sdk.spells.SpellStatistic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class DamageSpellTest {
 
@@ -29,6 +39,42 @@ public class DamageSpellTest {
 
         assertEquals(5, creatureForTesting.getAmount());
         assertEquals(180, creatureForTesting.getCurrentHp());
+    }
+
+    @Test
+    void spellShouldDealDamageIfCreatureIsInRange(){
+        Creature c1 = spy(Creature.class);
+        Creature c2 = spy(Creature.class);
+        Creature c3 = spy(Creature.class);
+        Creature c4 = spy(Creature.class);
+        GameEngine engine = new GameEngine(new Hero(List.of(c1,c2,c3,c4)), new Hero(new ArrayList<>()));
+        EconomySpell toCovert = new EconomySpell(SpellStatistic.FIRE_BALL);
+        DamageSpell spell = (DamageSpell) new DamageSpellFactory().createInner(toCovert, 1, new SpellMasteries());
+
+        engine.castSpell(spell,new Point(0,3));
+
+        verify(c2).applyDamage(anyInt());
+        verify(c1).applyDamage(anyInt());
+        verify(c3).applyDamage(anyInt());
+        verify(c4, never()).applyDamage(anyInt());
+    }
+
+    @Test
+    void spellShouldDealDamageIfCreatureIsInRange2(){
+        Creature c1 = spy(Creature.class);
+        Creature c2 = spy(Creature.class);
+        Creature c3 = spy(Creature.class);
+        Creature c4 = spy(Creature.class);
+        GameEngine engine = new GameEngine(new Hero(List.of(c1,c2,c3,c4)), new Hero(new ArrayList<>()));
+        EconomySpell toCovert = new EconomySpell(SpellStatistic.FIRE_BALL);
+        DamageSpell spell = (DamageSpell) new DamageSpellFactory().createInner(toCovert, 1, new SpellMasteries());
+
+        engine.castSpell(spell,new Point(2,2));
+
+        verify(c1).applyDamage(anyInt());
+        verify(c2).applyDamage(anyInt());
+        verify(c3).applyDamage(anyInt());
+        verify(c4, never()).applyDamage(anyInt());
     }
 
 
